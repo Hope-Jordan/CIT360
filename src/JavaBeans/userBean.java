48a23ef125f9850a1ab3b4ab631550c0ff2bb225
jordanhope <jordanbhope@gmail.com>
2016-02-20 15:44:34 -0700

    Added constructor to userBean

--- src/JavaBeans/userBean.java
+++ src/JavaBeans/userBean.java
@@ -7,6 +7,11 @@ public class userBean {
     private String username;
     private String password;
 
+    public userBean(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
